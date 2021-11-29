b08b31c03015a362b60b12a878a2cd563ed1777a
Daniela <dhia.katz@gmail.com>
2021-11-29 16:07:06 -0300

    Exercicios variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,41 @@
+/*Exercicio de Interpretação 1°
+let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a, b)
+10
+10 5 */
+
+/*Exercicio de Interpretação 2°
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+10 10 10 */
+
+/*Exercicio de Interpretação 3°
+let p = prompt("horasTrabalhadasPorDia?")
+let t = prompt("diatrabalhado?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+
+console.log("Exercicio Soma")
+
+let contaCorrente = 250;
+let poupanca = 900;
+console.log (contaCorrente + poupanca);
+
+console.log("Exercicio Multiplicação")
+
+let caixaDeOvos = 2
+let quantidadeDeOvos = 30
+console.log (caixaDeOvos * quantidadeDeOvos)
+
+
+
+
+
