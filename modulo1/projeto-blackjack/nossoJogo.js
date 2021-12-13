618054a03c35431ed04a19e4f6508b864d3aba5c
Daniela <dhia.katz@gmail.com>
2021-12-13 08:00:23 -0300

    Exercicios projeto blackjack editado

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,23 @@
  * 
  * 
  */
+//MONTANDO O JOGO
+const mensagemBoasVindas = prompt ("Boas vindas ao jogo de Blackjack!");
+var botaoOk = true
+var botaoCancel = false
+if(confirm("Quer iniciar uma nova rodada?")){
+   console.log(botaoOk)
+}else{
+   console.log("O jogo acabou!")
+   console.log(botaoCancel)
+}
+
+const carta = comprarCarta(); 
+// Sorteia uma carta. Por exemplo, o rei de ouros
+
+console.log(carta.texto) 
+// imprime o texto da carta. Nesse caso: "K♦️"
+
+console.log(carta.valor) 
+// imprime o valor da carta (um número). Nesse caso: 10)
+comprarCarta()
