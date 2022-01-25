13dd7d4cb408de49827e9668c8da0f991a6038dc
Daniela <dhia.katz@gmail.com>
2022-01-25 20:50:29 -0300

    Exercicios labenusers API falta finalizar

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import NovoCadastro from './Componentes/NovoCadastro';
+
+
+export default class App extends React.Component{
+render(){
+  return(
+  < >
+  <NovoCadastro/>
+  </> 
+
+    
+  )
+}
+}
